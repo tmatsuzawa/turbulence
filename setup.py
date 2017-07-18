2cf376056926b524c17fe841188b5a7a032ea172
npmitchell <noah.prentice.mitchell@gmail.com>
2017-07-18 10:56:07 -0500

    add setup file for packaging

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='turbulence',
+    version='0.1',
+    description='Misc utilities for turbulence',
+#      url='needs a URL',
+    author='Irvine Lab (originally Stephane)',
+    author_email='npmitchell@uchicago.edu',
+    license='MIT',
+    packages=['turbulence'],
+    zip_safe=False,
+#      package_data={'tangle': ['cl_src/*.cl']})
+)
